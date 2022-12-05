d46cab1abb9566394e6bb83dc96da4649cdf9076
Ashish Jain <65340283+ashishnjain@users.noreply.github.com>
2022-12-05 22:00:37 +0530

    Add files via upload

---
+++ GCalIntegration/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Google.Apis.Auth.OAuth2;
+
+using Google.Apis.Calendar;
+
+namespace GCalIntegration.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GCalIntegration/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GCalIntegration.MvcApplication" Language="C#" %>
